b9bf20e97cfb651a7087ddc3334b2ffe3ab6e288
Joel Teichroeb <joel@teichroeb.net>
2022-12-25 18:30:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         auto-theresmore
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       klusark
 // @match        https://www.theresmoregame.com/play/
@@ -13,6 +13,29 @@
 
     var resources = {
 
+    }
+
+    function getCurrentTab() {
+        var tabs = document.getElementById("main-tabs")
+        var buttons = tabs.getElementsByTagName("button")
+        for (var i = 0; i < buttons.length; ++i) {
+            var button = buttons[i]
+            if (button.ariaSelected == "true") {
+                return button.childNodes[1].textContent
+            }
+        }
+    }
+
+    function changeTab(name) {
+        var tabs = document.getElementById("main-tabs")
+        var buttons = tabs.getElementsByTagName("button")
+        for (var i = 0; i < buttons.length; ++i) {
+            var button = buttons[i]
+            if (button.childNodes[1].textContent == name) {
+                button.click();
+                return;
+            }
+        }
     }
 
     function getResources() {
@@ -43,19 +66,15 @@
             var row = rtab.children[i]
             var resource = row.children[0].innerHTML.split(" ")[0]
             var neededhtml = row.children[1].innerHTML
-            var has = false
-            if (neededhtml.indexOf("(") == -1) {
-                has = true
-            //    continue
-            }
             var split = neededhtml.split(" ")
             var needed = parseInt(split[0].replaceAll(",",""))
 
-
-            var val = "stuff"
-            if (resource in resources) {
-                val = Math.round(needed / resources[resource].rate)
+            if (!resource in resources) {
+                continue
             }
+
+            var val = Math.round(needed / resources[resource].rate)
+
             row.children[0].innerHTML = resource + " ["+val+"]"
         }
     }
@@ -68,7 +87,7 @@
     };
 
     const observer = new MutationObserver((m) => {
-        console.log("Body Observe", m)
+        //console.log("Body Observe", m)
         for (var i = 0; i < m.length; ++i) {
             if (m[i].addedNodes.length == 0) {
                 continue
@@ -82,13 +101,13 @@
             var content = contentl[0]
 
             const observer2 = new MutationObserver(function (m) {
-                console.log("Tooltip Observe", m)
+                //console.log("Tooltip Observe", m)
                 updateTooltipObserved(content)
             });
             getResources();
             updateTooltip(content);
-            if (content.children[0].childElementCount == 0) {
-                observer2.observe(content.children[0], {subtree: false, childList : true});
+            if (node.children[0].children[0].childElementCount == 0) {
+                observer2.observe(node.children[0].children[0], {subtree: false, childList : true});
             }
         }
     });
@@ -99,19 +118,27 @@
 
 
 
-    var html = `<div style="z-index: 9999;visibility: visible;position: absolute;inset: 0px auto auto 0px;margin: 0px;transform: translate(0px, 500px);">
-   <div class="tippy-box !max-w-[300px]" data-state="visible" tabindex="-1" data-animation="fade" role="tooltip" data-placement="bottom" style="max-width: 350px; transition-duration: 300ms;">
-      <div data-state="visible" style="transition-duration: 300ms;">
-         <div>
-            <p class="3xl:pt-2 text-base font-bold !text-violet-900 dark:!text-violet-300">Auto Settings</p>
-            <input type="checkbox" id="autobuyenabled" >
-			 <label for="autobuy">Choose a building:</label>
+    var html = `
+<div style="z-index: 9999;visibility: visible;position: absolute;inset: 0px auto auto 0px;margin: 0px;transform: translate(0px, 500px);">
+  <div class="tippy-box !max-w-[300px]" data-state="visible" tabindex="-1" data-animation="fade" role="tooltip" data-placement="bottom" style="max-width: 350px; transition-duration: 300ms;">
+    <div data-state="visible" style="transition-duration: 300ms;">
+      <div>
+        <p class="3xl:pt-2 text-base font-bold !text-violet-900 dark:!text-violet-300">Auto Settings</p>
+        <label for="autotabenabled">Auto Tab</label>
+        <input type="checkbox" id="autotabenabled" >
+        <br>
+        <label for="autoresearchenabled">Auto Research</label>
+        <input type="checkbox" id="autoresearchenabled" >
+        <br>
+        <label for="autobuyenabled">Auto Building</label>
+        <input type="checkbox" id="autobuyenabled" >
+        <br>
 
-             <select name="autobuy" id="autobuy" style="color: black; height: 300px" multiple>
-             </select>
-         </div>
+        <select name="autobuy" id="autobuy" style="color: black; height: 300px" multiple>
+        </select>
       </div>
-   </div>
+    </div>
+  </div>
 </div>`
 
     document.body.insertAdjacentHTML("beforeend", html)
@@ -133,35 +160,74 @@
             option.text = name;
             autobuy.add(option);
         }
-    }, 1000);
+    }, 100);
 
+    function processAuto(currentTab) {
+        var enabled, autobuy, container, buttons, i, j, name;
+        if (currentTab == "Build") {
+            enabled = document.getElementById("autobuyenabled")
+            autobuy = document.getElementById("autobuy")
+            if (!enabled.checked) {
+                return
+            }
 
-    setInterval(function () {
-        var enabled = document.getElementById("autobuyenabled")
-        var autobuy = document.getElementById("autobuy")
-        if (!enabled.checked) {
-            return
-        }
-
-        var container = document.getElementsByClassName("tab-container")[0]
-        var buttons = container.getElementsByTagName("button")
-        for (var i = 0; i < buttons.length; ++i) {
-            var name = buttons[i].childNodes[0].textContent
-            for (var j = 0; j < autobuy.selectedOptions.length; ++j) {
-                if (name != autobuy.selectedOptions[j].innerText) {
-                    continue
+            container = document.getElementsByClassName("tab-container")[0]
+            buttons = container.getElementsByTagName("button")
+            for (i = 0; i < buttons.length; ++i) {
+                name = buttons[i].childNodes[0].textContent
+                for (j = 0; j < autobuy.selectedOptions.length; ++j) {
+                    if (name != autobuy.selectedOptions[j].innerText) {
+                        continue
+                    }
+                    if (buttons[i].classList.contains("btn-off")) {
+                        continue;
+                    }
+                    if (buttons[i].classList.contains("btn-progress")) {
+                        break;
+                    }
+                    //console.log("Would click", name)
+                    buttons[i].click();
+                    break;
                 }
+            }
+        } else if (currentTab == "Research") {
+            enabled = document.getElementById("autoresearchenabled")
+            if (!enabled.checked) {
+                return
+            }
+
+            container = document.getElementsByClassName("tab-container")[0]
+            buttons = container.getElementsByTagName("button")
+            for (i = 0; i < buttons.length; ++i) {
                 if (buttons[i].classList.contains("btn-off")) {
                     continue;
+                }
+                if (buttons[i].classList.contains("btn-progress")) {
+                    break;
                 }
                 //console.log("Would click", name)
                 buttons[i].click();
                 break;
             }
         }
+    }
+
+    setInterval(function () {
+        var currentTab = getCurrentTab()
+
+        processAuto(currentTab);
+
+        var enabled = document.getElementById("autotabenabled")
+
+        if (!enabled.checked) {
+            return
+        }
+        if (currentTab == "Build") {
+            changeTab("Research")
+        } else if (currentTab == "Research") {
+            changeTab("Build")
+        }
 
     }, 1000);
-
-
 
 })();
